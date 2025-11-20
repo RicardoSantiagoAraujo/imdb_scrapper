567f492f790e9b6be43bb39f0ac66d5218c89ef0
Ricardo Santiago Araújo <61381226+RicardoSantiagoAraujo@users.noreply.github.com>
2025-11-20 11:33:51 +0100

    Add save progress interval to config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 # Rows to scrap
 startAt=0
 stopAt= None # IF None, IT WILL GO TILL END OF FILE
+save_every_n_rows = 10 # Save progress every N rows in case of crash during long runs
 
 
 # File paths
